00bea43f40a513b02214c86b365dcbeb908261f5
Enes <=enesaygur387@gmail.com>
2022-05-19 00:21:32 +0300

    Form Islemleri

--- LaravelEgitimi/app/Http/Controllers/Ornek.php
+++ LaravelEgitimi/app/Http/Controllers/Ornek.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class Ornek extends Controller
 {
     function test($isim)
--- LaravelEgitimi/routes/web.php
+++ LaravelEgitimi/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Ornek;
+use App\Http\Controllers\Yonet;
+use App\Http\Controllers\Formislemleri;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -10,3 +12,8 @@ Route::get('/deneme',function (){
     return view('ornek');
 });
 Route::get("/test/{isim}", [Ornek::class,"test"]);
+
+Route::get('/web',[Yonet::class,'site'])->name('web');
+
+Route::get("/form",[Formislemleri::class,'gorunum']);
+Route::middleware('arakontrol')->post("/form-sonuc",[Formislemleri::class,'sonuc'])->name('sonuc');
